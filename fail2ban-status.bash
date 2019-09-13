12326c8b2e23f2e22e7fab80d5a954ded41a52ff
M K Gharzai <kabeer.gharzai@stryker.com>
2019-09-13 11:00:00 -0400

    new iptables , sshfs , and stunnel scripts

--- fail2ban-status.bash
+++ fail2ban-status.bash
@@ -1,4 +1,5 @@
-sudo fail2ban-client status sshd basic
+# dont use sudo with watch or journalctl will be messy
+fail2ban-client status sshd basic
 # sudo fail2ban-client status apache-auth basic
 # sudo fail2ban-client status apache-botsearch basic
 # sudo fail2ban-client status apache-noscript basic
