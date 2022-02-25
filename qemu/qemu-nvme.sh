32c66fd754d54268d3440d87bd6f3264268b01cf
M K Gharzai <kabeer@gharzai.net>
2022-02-25 02:04:20 -0800

    added qemu nvme script with 1 gpu, multi usb passthru

--- qemu/qemu-nvme.sh
+++ qemu/qemu-nvme.sh
@@ -1,73 +1,131 @@
 #!/usr/bin/env bash
 
-LABEL="CE7C70027C6FE425" # UUID: Windows
-DRIVE=$(lsblk -pf | grep ${LABEL} | awk '{print $1}' | grep -o '/dev/nvme[0-9][a-z][0-9]')            # -> /dev/nvme1n1
-#RIVE=$(lsblk -pf | grep ${LABEL} | awk '{print $1}' | grep -o '/dev/nvme[0-9][a-z][0-9][a-z][0-9]')  # -> /dev/nvme1n1p4
-#ISO0="/opt/qemu/iso/windows.iso"
-ISO1="/opt/qemu/iso/virtio.iso"
-NVRAM="/opt/qemu/nvram/windows-nvme.fd"
-OVMF="/usr/share/edk2-ovmf"
-OVMF_BIN="OVMF_CODE.fd"
-#VMF_BIN="OVMF_CODE.secboot.fd"
+# UPDATE ALL PCIe REFS (`lspci -nnk`) WHEN CHANGING HARDWARE
+# ❯ for i in (seq 1 9); lspci -nnk -s $i":" ; end
 
-printf "Booting from ${DRIVE}...\n"
+QDIR="/opt/qemu"
+
+# need to unbind vfio-pci to see device
+#LABEL="CE7C70027C6FE425" # UUID: Windows
+#DRIVE=$(lsblk -pf | grep ${LABEL} | awk '{print $1}' | grep -o '/dev/nvme[0-9][a-z][0-9]')            # -> /dev/nvme1n1
+##RIVE=$(lsblk -pf | grep ${LABEL} | awk '{print $1}' | grep -o '/dev/nvme[0-9][a-z][0-9][a-z][0-9]')  # -> /dev/nvme1n1p4
+DRIVE="/dev/nvme1n1"
+
+#SO0="${QDIR}/iso/windows.iso"
+#SO1="${QDIR}/iso/virtio.iso"
+
+NVRAM="${QDIR}/nvram/windows-nvme.fd"
+UEFI="${QDIR}/uefi/win-nvme.fd"
+
+#VMF_BIN="OVMF_CODE.secboot.fd"
+IGVT_ROM="${QDIR}/iso/vbios_gvt_uefi.rom"
+
+GPU_ROM="${QDIR}/gpu/rx580.rom"
+
+NET_SH="${QDIR}/net/ifup.sh"
+MAC="DE:AD:BE:EF:C1:E0"
+
+printf "Booting with drives:\n"
+media=( $ISO $ISO1 $ISO2 $DRIVE $DRIVE1 $DRIVE2 )
+for d in ${media[@]}; do
+	[[ $d ]] && printf '\t'$d'\n'
+done
 
 read -p "Continue? " -n 1 -r
 echo
 
 if [[ $REPLY =~ ^[Yy] ]]
 then
-	virsh nodedev-detach pci_0000_03_00_0
-	# rebinds pci device to use vfio-pci kmod
+    echo
+    # rebinds pci device to use vfio-pci kmod
 
-		
-	qemu-system-x86_64 \
-	    -snapshot                                                                                               \
-	    -enable-kvm                                            `# enable KVM optimizations                    ` \
-	    -global     ICH9-LPC.disable_s3=1                      `# silently hang without printing error msg    ` \
-	    -global     driver=cfi.pflash01,property=secure,value=on                                                \
-		-device     isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"         \
-	    -drive      file=${OVMF}/${OVMF_BIN},if=pflash,format=raw,unit=0,readonly=on                            \
-	    -drive      file=${NVRAM},if=pflash,format=raw                                                          \
-	    -boot       order=cd,menu=on                           `# boot the first vhd                          ` \
-	    -m          8G                                         `# provide reasonable amount of ram            ` \
-	    -cpu        host                                       `# emulate the host processor                  ` \
-	    -smp        $(nproc)                                   `# num  of cores guest is permitted, all cores ` \
-	    -machine    type=q35,accel=kvm                         `# modern chipset (PCIe, AHCI), hardware accel ` \
-	    -usbdevice  tablet                                     `# send USB mouse pos matching host cursor     ` \
-	    -device     intel-hda                                  `# audio controller                            ` \
-	    -device     hda-duplex                                 `# simulated sound output thru OS audio system ` \
-	    -vga        qxl                                                                                         \
-	    -spice      port=5930,disable-ticketing=on                                                              \
-	    -daemonize                                                                                              \
-        -device     vfio-pci,host=03:00.0,id=nvme0             `# requires root, no guest virtio drivers      ` \
-	   
-	   #-device     virtio-scsi-pci,id=scsi0                   `# need virtio-scsi-pci controller             ` \
-	   #-drive      file=${DRIVE},if=none,format=raw,discard=unmap,aio=native,cache=none,id=scsi0               \
-	   #-device     scsi-hd,drive=scsi0,bus=scsi0.0            `# virtio SCSI emulation for (TRIM), (NCQ).    ` \
-	   
-	
-	   #-display vnc=127.0.0.1:100                    `# [sdl | none | gtk | vnc]` \
-	   #-drive file=${DRIVE},format=raw,media=disk    `# load raw disk` \
-	   #-drive      file=${DRIVE},cache=none,if=virtio         `# set a virtual virtio-blk hdd with partition ` \
-	   #-drive      file=${ISO0},media=cdrom                   `# set a virtual cd-rom and use iso            ` \
-	   
-	   
-	   #-device scsi-hd,drive=scsi0,bus=scsi0.0       `# ... ` \
-	   # -device virtio-serial \
-	   # -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
-	   # -chardev spicevmc,id=spicechannel0,name=vdagent \
-	   # -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
-	   # -daemonize                                    `# don't start monitor, we connect using RDP` \
-	
-	if [[ $? -eq 0 ]];
-	then
-	    #spicy -h 127.0.0.1 -p 5930 & disown spicy;  # spice-gtk
-	    spicy -h 127.0.0.1 -p 5930
-	fi
+    # i915
+    #udo virsh nodedev-detach pci_0000_00_02_0
+        #-device     vfio-pci,host=00:02.0,id=igpu0,addr=0x2,romfile=${IGVT_ROM}
+    
+    #sudo virsh nodedev-detach pci_0000_00_01_0
+    #lspci -nnk -s 00:01.0 | grep -E "Subsystem|Kernel driver in use"
+    #echo
+   
+   	# RX 580: VGA
+    sudo virsh nodedev-detach pci_0000_01_00_0
+    lspci -nnk -s 01:00.0 | grep -E "Subsystem|Kernel driver in use"
+    echo
+    # RX 580: HDMI Audio
+    sudo virsh nodedev-detach pci_0000_01_00_1
+    lspci -nnk -s 01:00.1 | grep -E "Subsystem|Kernel driver in use"
+    echo
+    # Samsung NVMe
+    sudo virsh nodedev-detach pci_0000_03_00_0
+    lspci -nnk -s 03:00.0 | grep -E "Subsystem|Kernel driver in use"
+    echo
+    
+    echo "vfio devices detached"
+    echo
+
+        #-cpu        host,kvm=off,hv_vendor_id=null
+
+	args=(
+        -enable-kvm
+        -m          32G
+        -cpu        host,kvm=off,+invtsc,vmware-cpuid-freq=on
+        -smp        $(nproc)
+        -machine    type=q35,accel=kvm
+        -boot       order=cd,menu=on
+        -global     ICH9-LPC.disable_s3=1
+        -global     driver=cfi.pflash01,property=secure,value=on
+        -drive      file=${UEFI},if=pflash,format=raw
+        -drive      file=${NVRAM},if=pflash,format=raw
+        -device     isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+        -device		pcie-root-port,id=pcie.1,bus=pcie.0,addr=1c.0,slot=1,chassis=1,multifunction=on
+        -device     vfio-pci,host=01:00.0,id=gpu0,bus=pcie.1
+        -device     vfio-pci,host=01:00.1,id=hda0,bus=pcie.1
+        -device     vfio-pci,host=03:00.0,id=nvme0
+        -netdev     user,id=vmnic
+        -device     e1000,netdev=vmnic,mac=${MAC}
+        -usbdevice  tablet
+        -device     ivshmem-plain,memdev=ivshmem,bus=pcie.1
+        -object     memory-backend-file,id=ivshmem,share=on,mem-path=/dev/shm/looking-glass,size=128M
+        -spice      port=5930,disable-ticketing=on
+        -vga        none
+        -daemonize
+    )
+        #-device     ivshmem-plain,memdev=ivshmem,bus=pcie.0
+        #-object     memory-backend-file,id=ivshmem,share=on,mem-path=/dev/shm/looking-glass,size=128M
+        #-vga        none
+        #-spice      port=5930,disable-ticketing=on
+
+    usb=(
+          -device     qemu-xhci,id=xhci
+    	 `lsusb | grep -E "Microdia|SINOWEALTH|Audio-Technica|C-Media Electronics" | tr -d ':' | \
+    	  awk '{print "-device   usb-host,hostdevice=/dev/bus/usb/"$2"/"$4}'`
+    )
+
+    qemu-system-x86_64 ${args[@]} ${usb[@]}
+
+    #if [[ $? -eq 0 ]];
+    #then
+    #    spicy -h 127.0.0.1 -p 5930
+    #fi
 else
-	#❯ sudo virsh nodedev-reattach pci_0000_03_00_0
-	# Device pci_0000_03_00_0 re-attached
-	virsh nodedev-reattach pci_0000_03_00_0
-
+	read -p "Restore devices? " -n 1 -r
+	echo
+	
+	if [[ $REPLY =~ ^[Yy] ]]
+	then
+    	echo
+    	sudo virsh nodedev-reattach pci_0000_01_00_0
+    	sudo virsh nodedev-reattach pci_0000_01_00_1
+    	sudo virsh nodedev-reattach pci_0000_03_00_0
+    	echo "vfio devices reattached"
+    fi
 fi
+
+#IOMMU Group 1:
+#	00:01.0 PCI bridge [0604]: Intel Corporation 6th-10th Gen Core Processor PCIe Controller (x16) [8086:1901] (rev 0a)
+#	01:00.0 VGA compatible controller [0300]: Advanced Micro Devices, Inc. [AMD/ATI] Ellesmere [Radeon RX 470/480/570/570X/580/580X/590] [1002:67df] (rev e7)
+#	01:00.1 Audio device [0403]: Advanced Micro Devices, Inc. [AMD/ATI] Ellesmere HDMI Audio [Radeon RX 470/480 / 570/580/590] [1002:aaf0]
+
+#   https://wiki.gentoo.org/wiki/GPU_passthrough_with_libvirt_qemu_kvm#QEMU
+#   > a dedicate root port other than pcie.0 is required by amd gpu for windows driver
+
