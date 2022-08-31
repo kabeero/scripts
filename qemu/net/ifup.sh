14bca847563645ea214ecda131096b5440564f53
M K Gharzai <kabeer@gharzai.net>
2022-08-30 19:44:47 -0700

    updated qemu net script

--- qemu/net/ifup.sh
+++ qemu/net/ifup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-set -x
+
+# set -x
 
 switch=br0
 
-if [ -n "$1" ];then
-    #udo ip tuntap add $1 mode tap user `whoami`
+if [[ $# -eq 1 ]]; then
     sudo ip link set $switch up
     sudo dhcpcd $switch
     sleep 0.5s
-    sudo ip tuntap add $1 mode tap group qemu
+    sudo ip tuntap add dev $1 mode tap group qemu
     sudo ip link set $1 up
     sleep 0.5s
     sudo ip link set $1 master $switch
@@ -17,4 +17,3 @@ else
     echo "Error: no interface specified"
     exit 1
 fi
-
