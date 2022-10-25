2b80bda3c69c004ccf1cd0e45db60642071cfa59
M K Gharzai <kabeer@gharzai.net>
2022-10-24 18:11:50 -0700

    updated maim-clip with cleanup

--- maim-clip.sh
+++ maim-clip.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 file=$(mktemp ~/screenshot-XXXXX.png)
-maim -uso $file; cat $file | xclip -selection clipboard -t image/png
+trap "rm $file; exit" 1 2 3 6
+maim -uso $file
+if [[ $? -eq 0 ]]; then
+    cat $file | xclip -selection clipboard -t image/png
+else
+    rm $file
+fi
