3505e365cd0a84734e8d5870f3982b1b713497f4
M K Gharzai <kabeer@gharzai.net>
2022-10-26 01:58:07 -0700

    maim clip script with timestamp

--- maim-clip.sh
+++ maim-clip.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env sh
-file=$(mktemp ~/screenshot-XXXXX.png)
+#!/usr/bin/env bash
+
+file="$HOME/screenshot-$(date +"%Y_%m_%d-%H_%M_%S").png"
+
+echo "📸 Saving screenshot to $file"
+
 trap "rm $file; exit" 1 2 3 6
+
 maim -uso $file
+
 if [[ $? -eq 0 ]]; then
     cat $file | xclip -selection clipboard -t image/png
 else
