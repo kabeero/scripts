e9ac89575e22da2d4ad527dd3d98ddc1e2f97d56
M K Gharzai <kabeer@gharzai.net>
2023-01-14 02:00:14 -0800

    updated kb repeat script

--- kb-repeat.sh
+++ kb-repeat.sh
@@ -3,10 +3,15 @@
 if [ $# -eq 0 ]; then
     printf "\nCurrent repeat rate\n\n"
     xset q | grep -E "^(\s)*auto repeat delay"
-else
-    DELAY=175
-    REPEAT=25
+else 
+    if [ $# -eq 2 ]; then
+        DELAY=$1
+        REPEAT=$2
+    else
+        DELAY=175
+        REPEAT=25
+    fi
     printf "\nSetting repeat rate\n\n"
     printf "  auto repeat delay:  %d    repeat rate:  %d\n" $DELAY $REPEAT
-    xset r rate 150 25
+    xset r rate $DELAY $REPEAT
 fi
