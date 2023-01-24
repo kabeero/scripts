2e4f260c81d027c95f036bab33a93a7261762d46
M K Gharzai <kabeer@gharzai.net>
2023-01-24 14:31:58 -0800

    updated s3 backup script

--- backup-s3.sh
+++ backup-s3.sh
@@ -3,6 +3,12 @@
 VAULT="${HOME}/Vault"
 S3_URI="s3://${USER}/"
 
+if [[ $# -eq 0 ]]; then
+    echo
+    echo "Please provide a command: upload | download"
+    exit 1
+fi
+
 if ! command -v isengardcli &> /dev/null
 then
     echo
@@ -10,26 +16,27 @@ then
     exit 1;
 fi
 
-if [[ $# -eq 0 ]]
-then
+eval $(isengardcli credentials ${USER})
+if [[ $? -ne 0 ]]; then
     echo
-    echo "Please provide a command: upload | download"
+    echo "🔐 Please re-authenticate with Midway"
     exit 1
-else
-    eval $(isengardcli credentials ${USER})
 fi
 
 case $1 in
 
     d*|download)
         echo
-        echo "🔃 Downloading changes from S3…"
+        echo "🔍 Comparing changes from S3…"
         echo
         aws s3 sync ${S3_URI} ${VAULT} --delete --exclude=".DS_Store" --exclude "Music/*" --dryrun
         echo
         echo
         read "REPLY?📚 Apply these changes? "
         if [[ $REPLY =~ "^[Yy]" ]]; then
+            echo
+            echo "🔃 Downloading changes from S3…"
+            echo
             aws s3 sync ${S3_URI} ${VAULT} --delete --exclude=".DS_Store" --exclude "Music/*"
         fi
         echo
@@ -37,12 +44,15 @@ case $1 in
 
     u*|upload)
         echo
-        echo "🔃 Uploading changes to S3…"
+        echo "🔎 Comparing changes to S3…"
         echo
         aws s3 sync ${VAULT} ${S3_URI} --delete --exclude=.DS_Store --exclude "Music/*" --dryrun
         echo
         read "REPLY?📚 Apply these changes? "
         if [[ $REPLY =~ "^[Yy]" ]]; then
+            echo
+            echo "🔃 Uploading changes to S3…"
+            echo
             aws s3 sync ${VAULT} ${S3_URI} --delete --exclude=.DS_Store --exclude "Music/*"
         fi
         echo
