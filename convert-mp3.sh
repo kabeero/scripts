88f861d200d3e26a859cf0b024bad1d1e4b4259f
M K Gharzai <kabeer@gharzai.net>
2023-02-04 01:36:36 -0800

    m4a -> mp3 fixes

--- convert-mp3.sh
+++ convert-mp3.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 if [[ $# -eq 2 ]]; then
-	ffmpeg -i $1 -c:v copy -c:a libmp3lame -q:a 4 $2
+	ffmpeg -i "$1" -c:v copy -c:a libmp3lame -q:a 4 "$2"
 else
 	echo "Please provide an input and output filename. (m4a -> mp3 by default)"
 fi
