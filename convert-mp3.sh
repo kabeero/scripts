2feb0fcfe3efc3ddae70d8c3fdeccaf12a51d84d
M K Gharzai <kabeer@gharzai.net>
2023-02-04 01:50:38 -0800

    updated m4a -> mp3 convert

--- convert-mp3.sh
+++ convert-mp3.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env sh
-if [[ $# -eq 2 ]]; then
-	ffmpeg -i "$1" -c:v copy -c:a libmp3lame -q:a 4 "$2"
+
+if [[ ! $(command -v ffmpeg) ]]; then
+	echo
+	echo "🔴 Please install ffmpeg"
+	echo
+	exit 1
+fi
+
+if [[ $# -gt 0 ]]; then
+	INFILE="$1"
+	echo
+	read -p "📻 Continue converting ${INFILE}? " REPLY
+	case $REPLY in
+		[yY]*)
+			echo
+			echo "🟢 Processing..."
+			echo
+			if [[ $# -eq 2 ]]; then
+				ffmpeg -i "$1" -c:v copy -c:a libmp3lame -q:a 4 "$2"
+			else
+				echo "💿 Saving ${1%%.m4a}.mp3"
+				echo
+				ffmpeg -i "$1" -c:v copy -c:a libmp3lame -q:a 4 "${1%%.m4a}.mp3"
+			fi
+			;;
+		*)
+			echo
+			echo "❌ Exiting"
+			;;
+	esac
 else
-	echo "Please provide an input and output filename. (m4a -> mp3 by default)"
+	echo
+	echo "🔴 Please provide an input and output filename. (m4a -> mp3 by default)"
+	echo
 fi
