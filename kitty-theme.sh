8bd9023e517d435f5daea99ff04fa755e112b2c9
M K Gharzai <kabeer@gharzai.net>
2023-11-29 19:15:32 -0800

    add bat color toggle to kitty theme

--- kitty-theme.sh
+++ kitty-theme.sh
@@ -5,29 +5,31 @@
 set -euo pipefail
 
 cmds=("kitty" "gum")
-for c in ${cmds[@]}; do
-    if [[ ! $(command -v "$c") ]]; then
-        echo "Please install $c"
-        exit 1
-    fi
+for c in "${cmds[@]}"; do
+	if [[ ! $(command -v "$c") ]]; then
+		echo "Please install $c"
+		exit 1
+	fi
 done
 
 if [[ $# -eq 0 ]]; then
-    mode=$(gum choose "Light" "Dark")
+	mode=$(gum choose "Light" "Dark")
 else
-    mode=$1
+	mode=$1
 fi
 
 case $mode in
-    Li*|l*)
-            echo "Using light mode"
-            kitty +kitten themes "GitHub Light"
-            ;;
-    Da*|d*)
-            echo "Using dark mode"
-            kitty +kitten themes "noirblaze"
-            ;;
-    *)
-            echo "Unknown theme"
-            ;;
+Li* | l*)
+	echo "Using light mode"
+	kitty +kitten themes "GitHub Light"
+	echo "--theme=OneHalfLight" >"${HOME}/.config/bat/config"
+	;;
+Da* | d*)
+	echo "Using dark mode"
+	kitty +kitten themes "noirblaze"
+	echo "--theme=OneHalfDark" >"${HOME}/.config/bat/config"
+	;;
+*)
+	echo "Unknown theme"
+	;;
 esac
