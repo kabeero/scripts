d50351854027f3384aaa6f3cbf6fc09fd239257c
M K Gharzai <kabeer@gharzai.net>
2024-02-01 18:29:27 -0800

    monitors update

--- monitors.sh
+++ monitors.sh
@@ -11,50 +11,76 @@ if ! command -v gum &>/dev/null; then
 	exit 1
 fi
 
+MONS=$(xrandr | grep -c " connected")
+
 if [[ $# -eq 1 ]]; then
-	select=$(echo $1 | grep -oE "^[0-9]")
+	select=$(echo "$1" | grep -oE "^[0-9]")
 else
 	echo
 	echo "🖵  Detected displays"
 	echo
-	displays=$(xrandr | grep " connected" | awk {'print $1'})
+	displays=$(xrandr | grep " connected" | awk '{print $1}')
 	for d in ${displays[@]}; do
 		echo "  $d"
 	done
 	echo
-	monitors=("1) Single monitor" "2) Dual monitor" "3) Triple monitor")
-	IFS="\\"
+	monitors=("0) Laptop monitor" "1) Single monitor" "2) Dual monitor")
+	if [[ $MONS == 3 ]]; then
+		monitors=("0) Laptop monitor" "1) Single monitor" "2) Dual monitor" "3) Triple monitor")
+	fi
+	IFS=$'..'
 	select=$(gum choose ${monitors[@]} | grep -oE "^[0-9]")
+	if [[ $? -ne 0 ]]; then
+		exit
+	fi
 fi
 
-echo $select
+echo "$select"
 
-# nvidia
-# INPUT1="DP-0"
-# INPUT2="DP-2"
-# INPUT3="DP-4"
-
-# amdgpu
-# INPUT1="DisplayPort-0"
-# INPUT2="DisplayPort-1"
-# INPUT3="DisplayPort-2"
-
-# generic
 set -exo pipefail
-INPUT1=$(xrandr | grep " connected" | awk '{if (NR==1) print $1}')
-INPUT2=$(xrandr | grep " connected" | awk '{if (NR==2) print $1}')
-INPUT3=$(xrandr | grep " connected" | awk '{if (NR==3) print $1}')
 
-if [[ $select == "1" ]]; then
-	xrandr --output "$INPUT1" --rotate left --auto
-	xrandr --output "$INPUT2" --off
-	xrandr --output "$INPUT3" --off
-elif [[ $select == "2" ]]; then
-	xrandr --output "$INPUT1" --rotate left --auto
-	xrandr --output "$INPUT2" --rotate right --auto --left-of "$INPUT1"
-	xrandr --output "$INPUT3" --off
-elif [[ $select == "3" ]]; then
-	xrandr --output "$INPUT1" --rotate left --auto
-	xrandr --output "$INPUT2" --rotate right --auto --left-of "$INPUT1"
-	xrandr --output "$INPUT3" --rotate left --auto --left-of "$INPUT2"
+if [[ $MONS == 2 ]]; then
+
+	INPUT1=$(xrandr | grep " connected" | grep -E "e?DP" | awk '{if (NR==1) print $1}')
+	INPUT2=$(xrandr | grep " connected" | grep -E "e?DP" | awk '{if (NR==2) print $1}')
+
+	if [[ $select == "0" ]]; then
+		xrandr --output "$INPUT1" --auto
+		xrandr --output "$INPUT2" --off
+	elif [[ $select == "1" ]]; then
+		xrandr --output "$INPUT1" --off
+		xrandr --output "$INPUT2" --rotate normal --auto
+	elif [[ $select == "2" ]]; then
+		xrandr --output "$INPUT1" --auto
+		xrandr --output "$INPUT2" --rotate normal --above "$INPUT1" --auto
+	fi
+
+elif [[ $MONS == 3 ]]; then
+
+	INPUT1=$(xrandr | grep " connected" | grep -E "e?DP" | awk '{if (NR==1) print $1}')
+	INPUT2=$(xrandr | grep " connected" | grep -E "e?DP" | awk '{if (NR==3) print $1}')
+	INPUT3=$(xrandr | grep " connected" | grep -E "e?DP" | awk '{if (NR==2) print $1}')
+
+	if [[ $select == "0" ]]; then
+		xrandr --output "$INPUT1" --auto
+		xrandr --output "$INPUT2" --off
+		xrandr --output "$INPUT3" --off
+	elif [[ $select == "1" ]]; then
+		xrandr --output "$INPUT1" --off
+		xrandr --output "$INPUT3" --rotate normal --auto
+	elif [[ $select == "2" ]]; then
+		xrandr --output "$INPUT1" --off
+		xrandr --output "$INPUT2" --rotate left --auto
+		xrandr --output "$INPUT3" --rotate right --auto --right-of "$INPUT2"
+	elif [[ $select == "3" ]]; then
+		xrandr --output "$INPUT1" --auto
+		xrandr --output "$INPUT2" --rotate left --auto --right-of "$INPUT1"
+		xrandr --output "$INPUT3" --rotate right --auto --right-of "$INPUT2"
+	fi
+else
+	echo "🟥 Unrecognized number of monitors"
 fi
+
+sleep 1
+
+background
