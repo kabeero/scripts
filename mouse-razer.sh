1a326e13e4291c61f6a91842b9d0e2991469af9c
M K Gharzai <kabeer@gharzai.net>
2024-02-02 17:37:56 -0800

    update razer script with logi

--- mouse-razer.sh
+++ mouse-razer.sh
@@ -1,31 +1,44 @@
 #!/usr/bin/env bash
 
-# set -xe
+set -uo pipefail
 
-MOUSE=$(gum choose Basilisk Deathadder)
-# MOUSE_STR="Razer Razer Mamba Tournament Edition"
-MOUSE_STR="Razer Razer Basilisk Ultimate Dongle"
-if [[ $MOUSE == "Deathadder" ]]; then
-	MOUSE_STR="Razer Razer DeathAdder V2 Pro"
+if [ ! "$(command -v gum)" ] &>/dev/null; then
+	echo
+	echo "❗ Please install gum"
+	echo
+	exit 1
 fi
-MOUSE_STR="${MOUSE_STR} [^CSK] .*pointer.*"
+
+MOUSE=$(gum choose Basilisk Deathadder Logitech)
+
+if [[ $MOUSE == "Basilisk" ]]; then
+	MOUSE_STR="Razer Razer Basilisk Ultimate Dongle"
+elif [[ $MOUSE == "Deathadder" ]]; then
+	MOUSE_STR="Razer Razer DeathAdder V2 Pro"
+elif [[ $MOUSE == "Logitech" ]]; then
+	MOUSE_STR="Logitech MX Ergo"
+fi
+
+if [[ $MOUSE =~ "Razer" ]]; then
+	MOUSE_STR="${MOUSE_STR} [^CSK] .*pointer.*"
+fi
 MOUSE_PROP="Natural Scrolling Enabled ("
 
-ID=$(xinput list | grep "slave  pointer" | grep -E "$MOUSE_STR" | awk '{print $8}' | sed -e 's/id=//g' | tail -1)
-NATURAL_ID=$(xinput list-props $ID | grep "$MOUSE_PROP" | sed -E 's/(.*)\(([0-9]{3})(.*)([0-9]{1})/\2/')
-NATURAL_ON=$(xinput list-props $ID | grep "$MOUSE_PROP" | sed -E 's/(.*)\(([0-9]{3})(.*)([0-9]{1})/\4/')
-BUTTON_MAP=$(xinput get-button-map $ID | awk '{print $6 $7}')
+ID=$(xinput list | grep "slave  pointer" | grep -E "$MOUSE_STR" | grep -oE "id=[0-9]+" | sed -e 's/id=//g' | tail -1)
+NATURAL_ID=$(xinput list-props "$ID" | grep "$MOUSE_PROP" | sed -E 's/(.*)\(([0-9]{3})(.*)([0-9]{1})/\2/')
+NATURAL_ON=$(xinput list-props "$ID" | grep "$MOUSE_PROP" | sed -E 's/(.*)\(([0-9]{3})(.*)([0-9]{1})/\4/')
+BUTTON_MAP=$(xinput get-button-map "$ID" | awk '{print $6 $7}')
 
 # print button map, shows side scroll left/right, etc
-button-info() { xinput list --long $ID | grep "Button labels" | sed -e 's/[\t]*Button labels: //g' | sed -e 's/" /\n/g' | sed -e 's/"//g' | sort; }
+button-info() { xinput list --long "$ID" | grep "Button labels" | sed -e 's/[\t]*Button labels: //g' | sed -e 's/" /\n/g' | sed -e 's/"//g' | sort; }
 
 # enable / disable / toggle fns
-side-on() { xinput set-button-map $ID 1 2 3 4 5 4 5 8 9 10 11 12 13 14 15 16 17 18 19 20 && echo "Side scrolling enabled"; }
-side-off() { xinput set-button-map $ID 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 && echo "Side scrolling disabled"; }
-side-flip() { [[ $BUTTON_MAP == "67" ]] && side-on || side-off; }
-natural-on() { xinput set-prop $ID $NATURAL_ID 1 && echo "Natural scrolling enabled"; }
-natural-off() { xinput set-prop $ID $NATURAL_ID 0 && echo "Natural scrolling disabled"; }
-natural-flip() { [ $NATURAL_ON -eq 0 ] && natural-on || natural-off; }
+side-on() { xinput set-button-map "$ID" 1 2 3 4 5 4 5 8 9 10 11 12 13 14 15 16 17 18 19 20 && echo "Side scrolling enabled"; }
+side-off() { xinput set-button-map "$ID" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 && echo "Side scrolling disabled"; }
+side-flip() { [[ "$BUTTON_MAP" == "67" ]] && side-on || side-off; }
+natural-on() { xinput set-prop "$ID" "$NATURAL_ID" 1 && echo "Natural scrolling enabled"; }
+natural-off() { xinput set-prop "$ID" "$NATURAL_ID" 0 && echo "Natural scrolling disabled"; }
+natural-flip() { [ "$NATURAL_ON" -eq 0 ] && natural-on || natural-off; }
 
 case $1 in
 
@@ -55,7 +68,7 @@ n | natural)
 			natural-flip
 		fi
 	else
-		[ $NATURAL_ON -eq 1 ] && echo "Natural scrolling enabled" || echo "Natural scrolling disabled"
+		[ "$NATURAL_ON" -eq 1 ] && echo "Natural scrolling enabled" || echo "Natural scrolling disabled"
 	fi
 	;;
 
