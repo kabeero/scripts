114097d532852e677ce17160e845da548e5333cf
M K Gharzai <kabeer@gharzai.net>
2024-04-20 12:59:23 -0700

    uvindex or weather

--- cli/uvindex.sh
+++ cli/uvindex.sh
@@ -1,1 +1,7 @@
-curl -s "wttr.in/?format=j1" | jq -r '.current_condition[] | ("UV Index: " + .uvIndex)'
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+	curl -s "wttr.in/92618?format=j1" | jq -r '.current_condition[] | ("UV Index: " + .uvIndex)'
+else
+	curl -s "wttr.in/92618?format=j1" | jq -r '.current_condition[]'
+fi
