b248b9b384027117ee8001a35b3de9150f45ec27
M K Gharzai <kabeer@gharzai.net>
2024-06-19 00:50:07 -0700

    kitty: theme select mods

--- kitty/kitty-theme.sh
+++ kitty/kitty-theme.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# toggle kitty's theme to light or dark
+# toggle kitty's theme, selector, light or dark, or fix tabs
 
 set -euo pipefail
+
+# DARKMODE="noirblaze"
+DARKMODE="Dark Pride"
+LIGHTMODE="GitHub Light"
 
 cmds=("kitty" "gum")
 for c in "${cmds[@]}"; do
@@ -12,8 +16,25 @@ for c in "${cmds[@]}"; do
 	fi
 done
 
+reload_kitty() {
+	pgrep -af kitty | grep "[k]itty -1" | awk '{print $1}' | xargs -n1 kill -USR1
+}
+
+fix_tabs() {
+	cat <<EOF >>"${HOME}/.config/kitty/current-theme.conf"
+active_tab_background   #111
+active_tab_foreground   #fff
+inactive_tab_background #222
+inactive_tab_foreground #666
+tab_bar_background      none
+EOF
+	reload_kitty
+}
+
 if [[ $# -eq 0 ]]; then
-	mode=$(gum choose "Light" "Dark")
+	# mode=$(gum choose "Light" "Dark")
+	kitty +kitten themes
+	exit 0
 else
 	mode=$1
 fi
@@ -23,13 +44,18 @@ mkdir -p "${HOME}/.config/bat"
 case $mode in
 Li* | l*)
 	echo "Using light mode"
-	kitty +kitten themes "GitHub Light"
+	kitty +kitten themes "$LIGHTMODE"
 	echo "--theme=OneHalfLight" >"${HOME}/.config/bat/config"
 	;;
 Da* | d*)
 	echo "Using dark mode"
-	kitty +kitten themes "noirblaze"
-	echo "--theme=OneHalfDark" >"${HOME}/.config/bat/config"
+	kitty +kitten themes "$DARKMODE"
+	echo "--theme=TwoDark" >"${HOME}/.config/bat/config"
+	fix_tabs
+	;;
+T* | t*)
+	echo "Fixing tab colors"
+	fix_tabs
 	;;
 *)
 	echo "Unknown theme"
