986d06363e8a7949dbbdb0b4018d4858b206bc47
M K Gharzai <kabeer@gharzai.net>
2025-05-23 16:14:56 -0700

    update bar and notch scripts

--- macos/notch.sh
+++ macos/notch.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
+export PURPLE=#4137ff
+export BLUE=#4c91ff
+
+export GUM_CHOOSE_CURSOR_FOREGROUND=$PURPLE
+export GUM_CHOOSE_HEADER_FOREGROUND=$BLUE
+export GUM_CHOOSE_SELECTED_FOREGROUND=$PURPLE
 export GUM_CHOOSE_SHOW_HELP=0
+
 choice=$(gum choose "With padding" "No padding")
+configfile="$HOME/.simplebarrc"
 
 echo "$choice"
 
+outer_padding=$(grep gap "${HOME}/.config/yabai/yabairc" | grep -o '[1-9][0-9]*')
+
+# set -x
 case $choice in
 With*)
-    yabai -m config top_padding 8
+    yabai -m config top_padding "$outer_padding"
+    gsed -i 's/"compactMode": false,/"compactMode": true,/' "$configfile"
+    osascript -e 'tell application id "tracesOf.Uebersicht" to refresh'
     ;;
 No*)
     yabai -m config top_padding 0
+    gsed -i 's/"compactMode": true,/"compactMode": false,/' "$configfile"
+    osascript -e 'tell application id "tracesOf.Uebersicht" to refresh'
     ;;
 esac
--- macos/simple-bar.sh
+++ macos/simple-bar.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
+export PURPLE=#4137ff
+export BLUE=#4c91ff
+
+export GUM_CHOOSE_CURSOR_FOREGROUND=$PURPLE
+export GUM_CHOOSE_HEADER_FOREGROUND=$BLUE
+export GUM_CHOOSE_SELECTED_FOREGROUND=$PURPLE
 export GUM_CHOOSE_SHOW_HELP=0
+
 choice=$(gum choose "Top" "Bottom")
 configfile="$HOME/.simplebarrc"
 
 echo "$choice"
 
+bar_height=$(jq -r .customStyles[] "$configfile" | grep bar-height | grep -o '[0-9]*')
+
+# set -x
 case $choice in
 Top*)
     gsed -i 's/"bottomBar": true,/"bottomBar": false,/' "$configfile"
+    gsed -i 's/"compactMode": false,/"compactMode": true,/' "$configfile"
     osascript -e 'tell application id "tracesOf.Uebersicht" to refresh'
-    yabai -m config external_bar all:28:0
+    # yabai -m config external_bar all:32:0
+    # yabai -m config external_bar all:64:0
+    yabai -m config external_bar "all:${bar_height}:0"
     ;;
 Bottom*)
     gsed -i 's/"bottomBar": false,/"bottomBar": true,/' "$configfile"
+    gsed -i 's/"compactMode": false,/"compactMode": true,/' "$configfile"
     osascript -e 'tell application id "tracesOf.Uebersicht" to refresh'
-    yabai -m config external_bar all:0:28
+    # yabai -m config external_bar all:0:34
+    # yabai -m config external_bar all:0:64
+    yabai -m config external_bar "all:0:${bar_height}"
     ;;
 esac
