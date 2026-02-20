1f295b1fac8af662d682f8a1fa0d24f83754bf8e
Kabeer Gharzai <kabeer@gharzai.net>
2026-02-19 18:38:09 -0800

    touchscreen: env bash

--- peripheral/monitor/touchscreen.sh
+++ peripheral/monitor/touchscreen.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/bash
-ID=`xinput list | grep SYNP1234 | awk '{print $5}' | sed -e 's/id=//g'`
-PROP=`xinput list-props ${ID} | grep -i "device enabled (" | awk '{print $3}' | sed -e "s/(//g" | sed -e "s/)://g"`
+#!/usr/bin/env bash
+ID=$(xinput list | grep SYNP1234 | awk '{print $5}' | sed -e 's/id=//g')
+PROP=$(xinput list-props ${ID} | grep -i "device enabled (" | awk '{print $3}' | sed -e "s/(//g" | sed -e "s/)://g")
 
-if [ -n "$1" ]
-then
-	xinput set-prop ${ID} ${PROP} $1
+if [ -n "$1" ]; then
+    xinput set-prop ${ID} ${PROP} $1
 else
-	echo $(xinput list-props ${ID} | grep ${PROP} | awk '{print $4}')
+    echo $(xinput list-props ${ID} | grep ${PROP} | awk '{print $4}')
 fi
